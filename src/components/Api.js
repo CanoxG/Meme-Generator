cd10722c09830a97ad7d0f4e25fc5073ffd28d59
Can Guven <bcanguven@gmail.com>
2022-11-20 20:52:44 +0000

    fetch Api

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+console.log("Component Rendered");
 
-function Api(props) {
+function Api() {
+  const [starWarsData, setStarWarsData] = useState({});
+  const [count, setCount] = useState(0);
+
+  // Side Effect
+  useEffect(() => {
+    if (!count) return;
+    fetch(`https://swapi.dev/api/people/${count}`)
+      .then((res) => res.json())
+      .then((data) => setStarWarsData(data));
+  }, [count]);
+
   return (
-    <div></div>
+    <>
+      <h3>The count is {count}</h3>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        Get Next Character
+      </button>
+      <br/>
+      {/* Conditional Rendering */ }
+      {count > 0 ? <pre>{JSON.stringify(starWarsData, null, 2)}</pre> : 'Loading'}
+    </>
   );
 }
 
