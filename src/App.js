f37594ceb76f4639769a6629857ad60ffd83a327
Can Guven <bcanguven@gmail.com>
2022-11-20 21:26:42 +0000

    added Tracker

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Meme from "./components/Meme";
+import Api from "./components/Api";
+import Tracker from "./components/Tracker";
 
 function App() {
+  const [show, setShow] = useState(true);
+
+  function toggle() {
+    setShow((prev) => !prev)
+  }
+
   return (
-    <div className="App">
-       <Main />
-       <Meme />
-      <br/>
-    </div>
+    <>
+      <button onClick={toggle}>Show</button>
+      <div className="flex-container">
+        <div className="App">
+          <Main />
+          <Meme />
+        </div>
+        <div className="App">
+          <Api />
+        </div>
+        <div className="App">
+          {show && <Tracker />}
+        </div>
+      </div>
+    </>
   );
 }
 
