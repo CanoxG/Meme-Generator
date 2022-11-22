f118b4ae1e7843e525edf21005cedc5e3d7287e4
Can Guven <bcanguven@gmail.com>
2022-11-22 21:23:16 +0000

    delete tracker

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Meme from "./components/Meme";
 import Api from "./components/Api";
 
 function App() {
-  const [show, setShow] = useState(true);
-
-  function toggle() {
-    setShow((prev) => !prev);
-  }
-
   return (
-    <>
-      <button onClick={toggle}>Show</button>
-      <div className="flex-container">
-        <div className="App">
-          <Main />
-          <Meme />
-        </div>
-        <div className="App">
-          <Api />
-        </div>
+    <div className="flex-container">
+      <div className="App">
+        <Main />
+        <Meme />
       </div>
-    </>
+      <div className="App">
+        <Api />
+      </div>
+    </div>
   );
 }
 
